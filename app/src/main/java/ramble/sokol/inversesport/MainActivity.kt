2bd174b79781c85bf6417da404ca75b99212a43a
RambleTOT <90986878+RambleTOT@users.noreply.github.com>
2023-11-28 11:58:56 +0500

    add ymap

--- app/src/main/java/ramble/sokol/inversesport/MainActivity.kt
+++ app/src/main/java/ramble/sokol/inversesport/MainActivity.kt
@@ -2,10 +2,42 @@ package ramble.sokol.inversesport
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MapKitFactory.setApiKey("a527d12d-2fd5-4eaf-9b11-4a301efad482")
+        MapKitFactory.initialize(this)
+        setContentView(R.layout.activity_main)
+
+        val splashScreenFragment = SplashScreenFragment()
+        val fragment : Fragment? =
+
+            supportFragmentManager.findFragmentByTag(SplashScreenFragment::class.java.simpleName)
+
+        if (fragment !is SplashScreenFragment){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.layout_fragment, splashScreenFragment, SplashScreenFragment::class.java.simpleName)
+                .commit()
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
 }
