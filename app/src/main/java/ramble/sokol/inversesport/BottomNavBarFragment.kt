b9fc336bcd2e49bdf802aed425f72a890175ac0d
RambleTOT <90986878+RambleTOT@users.noreply.github.com>
2023-11-28 13:00:35 +0500

    create navbar

--- app/src/main/java/ramble/sokol/inversesport/BottomNavBarFragment.kt
+++ app/src/main/java/ramble/sokol/inversesport/BottomNavBarFragment.kt
@@ -5,55 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ramble.sokol.inversesport.databinding.FragmentBottomNavBarBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BottomNavBarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BottomNavBarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding: FragmentBottomNavBarBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_nav_bar, container, false)
+        binding =FragmentBottomNavBarBinding.inflate(inflater, container, false)
+        val view = binding!!.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BottomNavBarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BottomNavBarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        replaceFragment(AfishaFragment())
+        binding!!.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.navbar_afisha -> replaceFragment(AfishaFragment())
+                R.id.navbar_map -> replaceFragment(MapFragment())
+                else -> {}
             }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = parentFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.frame_layout, fragment)
+        fragmentTransition.commit()
+
+    }
+
 }
