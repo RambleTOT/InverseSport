43144cf280e62386f85ad0236e36075e7930eac5
RambleTOT <90986878+RambleTOT@users.noreply.github.com>
2023-11-28 23:52:35 +0500

    create UI sign up

---
+++ app/src/main/java/ramble/sokol/inversesport/SignupFragment.kt
@@ -0,0 +1,23 @@
+package ramble.sokol.inversesport
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ramble.sokol.inversesport.databinding.FragmentSignupBinding
+
+class SignupFragment : Fragment() {
+
+    private var binding: FragmentSignupBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
+        val view = binding!!.root
+        return view
+    }
+}
